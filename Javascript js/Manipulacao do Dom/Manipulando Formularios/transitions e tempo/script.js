0296f376c4f6df69f26964f8b3b4d9df30abf8e9
Wendersonjose <wenderson.teles@icloud.com>
2025-08-27 14:19:50 -0300

    feat: correcao

--- Javascript js/Manipulacao do Dom/Manipulando Formularios/transitions e tempo/script.js
+++
@@ -1,8 +0,0 @@
-const btn = document.getElementById('button');
-const container = document.getElementById('container');
-
-btn.addEventListener("click", function() {
-    container.classList.toggle("red")
-    container.classList.toggle("blue")
-    container.classList.toggle("animacao-mover")
-})
